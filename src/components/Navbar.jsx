f5a2ffcd0cd4d08b4430c1e5c972bb5f68d2e824
Omar Chiruco <chirucoomar90@gmail.com>
2026-08-29 14:50:29 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ export const Navbar = () => {
            <ThemeToggle />
         <button
           onClick={() => setIsMenuOpen((prev) => !prev)}
-          className="md:hidden p-2 text-foreground z-50"
+          className="p-2 text-foreground z-50"
           aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
         >
           {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
